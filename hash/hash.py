f39019d91af95ad9e996b7409ab7060c63223e77
Erick <erick.miyagi@sptech.school>
2025-07-10 10:42:02 -0300

    feat: hash table, map and reltationship

---
+++ hash/hash.py
@@ -0,0 +1,16 @@
+caderno = dict()
+list_telephone = dict()
+
+caderno["maça"] = 2.44
+caderno["leite"] = 1.49
+caderno["ovos"] = 0.79
+caderno["abacate"] = 1.49
+
+list_telephone["jon"] = 867261283
+list_telephone["billy"] = 98612532
+
+print(caderno)
+print(caderno["maça"])
+
+print(list_telephone)
+print(list_telephone["jon"])
